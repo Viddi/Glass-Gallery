77d5dc11de9cc77e6ed9e04f4c461760b36a0b91
Jon Staff <jstaff@indatus.com>
2014-01-30 14:20:05 -0500

    Progress on the VideoActivity

--- main/src/main/java/is/vidarottosson/glass/gallery/video/VideoActivity.java
+++ main/src/main/java/is/vidarottosson/glass/gallery/video/VideoActivity.java
@@ -3,6 +3,7 @@ package is.vidarottosson.glass.gallery.video;
 //  Created by jonstaff on 1/17/14.
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
@@ -10,13 +11,17 @@ import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 import com.google.android.glass.widget.CardScrollView;
 
 import java.io.File;
@@ -30,7 +35,7 @@ import is.vidarottosson.glass.gallery.models.FileItem;
 import is.vidarottosson.glass.gallery.models.VideoItem;
 import is.vidarottosson.glass.gallery.util.Utility;
 
-public class VideoActivity extends Activity {
+public class VideoActivity extends Activity implements GestureDetector.BaseListener {
 	public static final String TAG = VideoActivity.class.getSimpleName();
 
 	private VideoItem mVideo;
@@ -38,6 +43,15 @@ public class VideoActivity extends Activity {
 	private VideoView mVideoView;
 	private TextView mTxtView;
 	private ProgressBar mProgressBar;
+
+	private GestureDetector mDetector;
+
+	//     _     _  __                      _
+	//    | |   (_)/ _| ___  ___ _   _  ___| | ___
+	//    | |   | | |_ / _ \/ __| | | |/ __| |/ _ \
+	//    | |___| |  _|  __/ (__| |_| | (__| |  __/
+	//    |_____|_|_|  \___|\___|\__, |\___|_|\___|
+	//                           |___/
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,19 +68,66 @@ public class VideoActivity extends Activity {
 		}
 
 		mVideoView = (VideoView) findViewById(R.id.video_videoView);
+		mVideoView.setVideoPath(mVideo.getPath());
+
 		mTxtView = (TextView) findViewById(R.id.video_textView);
+		mTxtView.setText(mVideo.getName());
+
 		mProgressBar = (ProgressBar) findViewById(R.id.video_progressBar);
+		// FIXME: ^^ is this animating by default?
 
-        mVideoView.setVideoPath(mVideo.getPath());
+		MediaController controller = new MediaController(this);
+		controller.setAnchorView(mVideoView);
+		mVideoView.setMediaController(controller);
+		mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+			@Override
+			public void onPrepared(MediaPlayer mediaPlayer) {
+				mVideoView.start();
+				mProgressBar.setVisibility(View.GONE);
+			}
+		});
 
-        MediaController controller = new MediaController(this);
-        controller.setAnchorView(mVideoView);
-        mVideoView.setMediaController(controller);
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mVideoView.start();
-            }
-        });
+		mDetector = new GestureDetector(this).setBaseListener(this);
+	}
+
+	//     ___       _             __                  ___                 _                           _        _   _
+	//    |_ _|_ __ | |_ ___ _ __ / _| __ _  ___ ___  |_ _|_ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_(_) ___  _ __  ___
+	//     | || '_ \| __/ _ \ '__| |_ / _` |/ __/ _ \  | || '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \/ __|
+	//     | || | | | ||  __/ |  |  _| (_| | (_|  __/  | || | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | \__ \
+	//    |___|_| |_|\__\___|_|  |_|  \__,_|\___\___| |___|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|___/
+	//                                                              |_|
+
+	@Override
+	public boolean onGenericMotionEvent(MotionEvent event) {
+		return mDetector.onMotionEvent(event);
+	}
+
+	@Override
+	public boolean onGesture(Gesture gesture) {
+		Log.i(TAG, "the gesture is: " + gesture);
+		if (gesture == Gesture.TAP || gesture == Gesture.LONG_PRESS) {
+			showMenu();
+			return true;
+		}
+
+		if (gesture == Gesture.TWO_TAP) {
+			togglePlaying();
+		}
+
+		return false;
+	}
+
+	//     ____       _            _         __  __      _   _               _
+	//    |  _ \ _ __(_)_   ____ _| |_ ___  |  \/  | ___| |_| |__   ___   __| |___
+	//    | |_) | '__| \ \ / / _` | __/ _ \ | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|
+	//    |  __/| |  | |\ V / (_| | ||  __/ | |  | |  __/ |_| | | | (_) | (_| \__ \
+	//    |_|   |_|  |_| \_/ \__,_|\__\___| |_|  |_|\___|\__|_| |_|\___/ \__,_|___/
+
+	private void togglePlaying() {
+		// TODO: implement playing
+	}
+
+	private void showMenu() {
+		// TODO: implement menu
 	}
 }
