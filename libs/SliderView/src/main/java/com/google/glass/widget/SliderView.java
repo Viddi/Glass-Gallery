ff1f34a25e3440130541f9cbdabcc7c1dd42eeb7
Viðar Ingi Ottósson <viddi@nplexity.com>
2016-04-19 22:04:03 -0700

    Updated gradle

--- libs/SliderView/src/main/java/com/google/glass/widget/SliderView.java
+++ libs/SliderView/src/main/java/com/google/glass/widget/SliderView.java
@@ -8,7 +8,6 @@ import android.animation.TimeInterpolator;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
